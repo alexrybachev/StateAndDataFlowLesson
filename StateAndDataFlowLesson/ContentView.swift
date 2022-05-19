6a559749de406e8894044f9f706c8dc88c987604
Alex Rybachev <rybachev.as@gmail.com>
2022-05-19 07:48:31 +0400

    Initial Commit

---
+++ StateAndDataFlowLesson/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StateAndDataFlowLesson
+//
+//  Created by Aleksandr Rybachev on 19.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StateAndDataFlowLesson/StateAndDataFlowLessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowLessonApp.swift
+//  StateAndDataFlowLesson
+//
+//  Created by Aleksandr Rybachev on 19.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowLessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
