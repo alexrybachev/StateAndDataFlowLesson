d16eb402e75c9579b38c910d8087f48f5ef3abdb
Alex Rybachev <rybachev.as@gmail.com>
2022-05-19 13:03:24 +0400

    setup methods for button

--- StateAndDataFlowLesson/TimeCounter.swift
+++ StateAndDataFlowLesson/TimeCounter.swift
@@ -13,16 +13,20 @@ class TimeCounter: ObservableObject {
     let objectWillChange = PassthroughSubject<TimeCounter, Never>()
     
     var counter = 3
+    var buttonTitle = "Start"
     var timer: Timer?
     
     func startTimer() {
-        timer = Timer.scheduledTimer(
-            timeInterval: 1,
-            target: self,
-            selector: #selector(updateCounter),
-            userInfo: nil,
-            repeats: true
-        )
+        if counter > 0 {
+            timer = Timer.scheduledTimer(
+                timeInterval: 1,
+                target: self,
+                selector: #selector(updateCounter),
+                userInfo: nil,
+                repeats: true
+            )
+        }
+        buttonDidTapped()
     }
     
     @objc private func updateCounter() {
@@ -30,6 +34,17 @@ class TimeCounter: ObservableObject {
             counter -= 1
         } else {
             killTimer()
+            buttonTitle = "Reset"
+        }
+        objectWillChange.send(self)
+    }
+    
+    private func buttonDidTapped() {
+        if buttonTitle == "Reset" {
+            counter = 3
+            buttonTitle = "Start"
+        } else {
+            buttonTitle = "Wait.."
         }
         objectWillChange.send(self)
     }
